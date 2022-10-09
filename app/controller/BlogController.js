ae66654b376e122bc218fd7c3ab8bb698769f5e9
swatantra <swatantra0723@gmail.com>
2022-10-09 13:20:17 +0545

    validation

--- app/controller/BlogController.js
+++ app/controller/BlogController.js
@@ -1,8 +1,32 @@
+import Blog from '../models/Blog.js';
+import { body, check, validationResult } from 'express-validator';
 export const index = async ( req, res ) => {
 	return res.send( 'hello blogs nodemon' );
 };
 
 export const store = async ( req, res ) => {
-	console.log( req.body );
-	return res.send( 'hello blogs store' );
+	try
+	{
+		const errors = validationResult( req );
+		if ( errors )
+		{
+			return res.status( 422 ).json( { errors: errors.array() } );
+		}
+
+		const { title, description } = req.body;
+
+		const blog = new Blog( {
+			title,
+			description,
+		} );
+
+		await blog.save();
+
+		res.json( { message: "Success" } );
+
+	} catch ( error )
+	{
+		console.log( error );
+	}
+
 };
